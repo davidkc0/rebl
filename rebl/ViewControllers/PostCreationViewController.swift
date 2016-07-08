af7501586fdab39030058da229ca33ce527402a5
VinceFilary <vbf711@gmail.com>
2016-07-07 17:08:08 -0700

    added Post Parse connectivity

--- rebl/ViewControllers/PostCreationViewController.swift
+++ rebl/ViewControllers/PostCreationViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import Parse
 
 class PostCreationViewController: UIViewController {
 
+    @IBOutlet weak var urlTextBox: UITextField!
+    @IBOutlet weak var postTitleTextBox: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func submitButton(sender: AnyObject) {
+        let postObject = PFObject(className: "Post")
+        
+        postObject["url"] = urlTextBox.text
+        postObject["title"] = postTitleTextBox.text
+        
+        postObject["user"] = PFUser.currentUser()
+        
+        postObject.saveInBackgroundWithBlock(nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +37,8 @@ class PostCreationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     
 
     /*
